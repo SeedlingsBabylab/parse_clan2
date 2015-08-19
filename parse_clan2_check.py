697a5a186ea0b511e5828950d4a4a6731e43d75b
Andrei Amatuni <andrei.amatuni@gmail.com>
2015-08-19 09:56:01 -0400

    Print words missing from clan freq output
    The output now shows words that were included in the parse_clan2 output
    but not in the can freq command output. These are words clan missed but
    parse_clan2 didn't.

--- parse_clan2_check.py
+++ parse_clan2_check.py
@@ -36,7 +36,9 @@ if __name__ == "__main__":
     parse_clan_count(sys.argv[1])
     parse_parseclan_output(sys.argv[2])
 
-    missing_words = []
+    missing_words_parse = []
+    missing_words_clan = []
+
     clan_count_sum = 0
 
     parseclan_counted = Counter(parseclan_words)
@@ -44,14 +46,25 @@ if __name__ == "__main__":
     for entry in clan_counts:
         clan_count_sum += entry[0]
         if entry[1] not in parseclan_words:
-            missing_words.append(entry[1])
+            missing_words_parse.append(entry[1])
             continue
         if parseclan_counted[entry[1]] != entry[0]:
-            missing_words.append(entry[1])
+            missing_words_parse.append(entry[1])
+
+    for entry in parseclan_words:
+        #clan_count_sum += entry[0]
+        if entry not in [count[1] for count in clan_counts]:
+            missing_words_clan.append(entry)
+            continue
+        if parseclan_counted[entry] != [count[0] for count in clan_counts]\
+                [[count[1] for count in clan_counts].index(entry)]:
+            missing_words_clan.append(entry)
 
 
-    print "\nmissing words: " + str(missing_words)
-    print
+    print parseclan_counted
+    print "\nmissing from parse_clan2 output: {}".format(missing_words_parse)
+    print "\nmissing from clan freq (but included in csv parse): {}\n".format(missing_words_clan)
+
     print "# of words (clan): " + str(clan_count_sum)
     print "# of words (parse_clan2): " + str(len(parseclan_words))
 
